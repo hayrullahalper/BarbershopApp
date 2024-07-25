889dd13ce6e54503a4470a47bf3212b4d26bcb9e
Hayrullah <alperhayrullah@gmail.com>
2024-07-25 14:25:12 +0300

    feat: add auth service

---
+++ barbershopapp/android/app/src/main/kotlin/com/metacortx/barbershop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.metacortx.barbershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- barbershopapp/android/app/src/main/kotlin/com/metacortx/barbershopapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.metacortx.barbershopapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
